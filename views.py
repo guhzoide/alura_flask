0da9ba596177b105dda84c357368526ce1d2b705
guhzoide <hugorobertiferreira61.br@gmail.com>
2024-02-21 18:33:11 -0300

    Finalizando as funcoes CRUD e incluindo a opcao de upload de imagens

--- views.py
+++ views.py
@@ -25,21 +25,45 @@ def criar():
         flash("Este jogo já consta no banco de dados")
         return redirect(url_for('index'))
 
-    else:
-        novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
-        db.session.add(novo_jogo)
-        db.session.commit()
-        return redirect(url_for('index'))
 
-@app.route('/editar')
-def editar():
+    novo_jogo = Jogos(nome=nome, categoria=categoria, console=console)
+    db.session.add(novo_jogo)
+    db.session.commit()
+
+    arquivo = request.files['arquivo']
+    arquivo.save(f'uploads/{arquivo.filename}')
+
+    return redirect(url_for('index'))
+
+@app.route('/editar/<int:id>')
+def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('editar')))
-    return render_template('editar.html', titulo='Editando Jogo')
+    jogo = Jogos.query.filter_by(id=id).first()
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
 
 @app.route('/atualizar', methods=['POST', 'GET'])
 def atualizar():
-    pass
+    jogo = Jogos.query.filter_by(id=request.form['id']).first()
+    jogo.nome = request.form['nome']
+    jogo.categoria = request.form['categoria']
+    jogo.console = request.form['console']
+
+    db.session.add(jogo)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login'))
+    
+    Jogos.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    flash("Jogo deletado com sucesso!")
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
